d82b40491209f892eb295e166a87337f0740d7e9
Jesse Hantula <jesse.hantula@helsinki.fi>
2024-11-26 21:03:02 +0200

    Add images for cards

--- blackjack/game_logic.py
+++ blackjack/game_logic.py
@@ -1,5 +1,14 @@
 import random
 
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+        self.name = f"{rank.lower()}_of_{suit.lower()}"
+
+    def __repr__(self):
+        return self.name
+
 class BlackjackGame:
     def __init__(self):
         self.deck = self.create_deck()
@@ -11,8 +20,8 @@ class BlackjackGame:
         # We want to make a deck consisting of 6 sets of 52 cards
         # This is the standard amount used in casinos and online games
         suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-        deck = [{'rank': rank, 'suit': suit} for _ in range(6) for suit in suits for rank in ranks]
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        deck = [Card(rank, suit) for _ in range(6) for suit in suits for rank in ranks]
         random.shuffle(deck)
         return deck
 
@@ -25,10 +34,10 @@ class BlackjackGame:
             hand.append(self.deck.pop())
 
     def calculate_score(self, hand):
-        values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
-        score = sum(values[card['rank']] for card in hand)
+        values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+        score = sum(values[card.rank] for card in hand)
         # Adjust for Aces
-        aces = sum(1 for card in hand if card['rank'] == 'A')
+        aces = sum(1 for card in hand if card.rank == 'Ace')
         while score > 21 and aces:
             score -= 10
             aces -= 1
@@ -70,8 +79,8 @@ class BlackjackGame:
 
     def get_game_state(self):
         return {
-            'player_hand': self.player_hand,
-            'dealer_hand': self.dealer_hand,
+            'player_hand': [card.name for card in self.player_hand],
+            'dealer_hand': [card.name for card in self.dealer_hand],
             'player_score': self.calculate_score(self.player_hand),
             'dealer_score': self.calculate_score(self.dealer_hand),
             'game_over': self.game_over
