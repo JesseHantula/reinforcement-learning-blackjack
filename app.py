1293c29d3e23a00ea6852493e1e89421e5e7ece8
Jesse Hantula <jesse.hantula@helsinki.fi>
2024-11-30 18:43:39 +0200

    Remove player vs bot@

--- app.py
+++ app.py
@@ -8,41 +8,10 @@ simulation = None
 agent = RLAgent()
 
 @app.route('/')
-def home():
-    return render_template('blackjack.html')
-
-@app.route('/test')
 def test_bot():
     global simulation
     simulation = BlackjackGame()
     return render_template('simulation.html')
-
-@app.route('/start', methods=['POST'])
-def start_game():
-    global game
-    game = BlackjackGame()  # Reset the game
-    game.deal_player()
-    game.deal_dealer()
-    return jsonify(game.get_game_state())
-
-@app.route('/new_hand', methods=['POST'])
-def new_hand():
-    game.reset()
-    game.deal_player()
-    game.deal_dealer()
-    return jsonify(game.get_game_state())
-
-@app.route('/hit', methods=['POST'])
-def hit():
-    message = game.player_hit()
-    if game.game_over:
-        return jsonify({**game.get_game_state(), 'message': message, 'game_over': True})
-    return jsonify({**game.get_game_state(), 'message': message, 'game_over': False})
-
-@app.route('/stand', methods=['POST'])
-def stand():
-    message = game.dealer_turn()
-    return jsonify({**game.get_game_state(), 'message': message})
 
 @app.route('/simulate', methods=['POST'])
 def simulate():
